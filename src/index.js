c0ffd40752c4f24a6273c8d6bb30990bd8d13060
sharontakhar <sharon.takhar@moonpig.com>
2023-07-05 14:44:07 +0100

    add amplify imports

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -17,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default withAuthenticator(App);
